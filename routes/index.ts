ade6ed993e70f9757d73188d1532d535da768aeb
VictorLG21 <victorgaspar1@hotmail.com>
2021-05-31 15:50:11 -0300

    udate front e public

--- routes/index.ts
+++ routes/index.ts
@@ -4,15 +4,28 @@ import Usuario = require("../models/usuario");
 class IndexRoute {
   public async index(req: app.Request, res: app.Response) {
     res.render("index.ejs", {
-     
+      layout: "layout-home",
     });
   }
   
+  @app.route.methodName( app.root + "/cadastrar")
   public async cadastrar(req: app.Request, res: app.Response) {
     res.render("cadastrar.ejs", {
-     
+      layout: "layout-home",
     });
   }
+
+  @app.route.methodName( app.root + "/login")
+	public async dadosBanca(req: app.Request, res: app.Response) {
+		
+		
+			res.render("login.ejs", {
+				layout: "layout-home",
+	
+			});
+			return;
+		}
+
 
 }
 
